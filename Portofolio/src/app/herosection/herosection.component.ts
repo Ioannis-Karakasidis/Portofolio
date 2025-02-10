26dd2f00adc065f04bb6d777a0dc60dc18f229ec
Ioannis Karakasidis <168634993+Ioannis-Karakasidis@users.noreply.github.com>
2025-02-10 05:40:13 +0100

    .

--- Portofolio/src/app/herosection/herosection.component.ts
+++ Portofolio/src/app/herosection/herosection.component.ts
@@ -1,15 +1,24 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { HeaderlogoComponent } from '../headerlogo/headerlogo.component';
 import { ContacticonsComponent } from '../contacticons/contacticons.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-herosection',
   standalone: true,
-  imports: [HeaderlogoComponent,ContacticonsComponent],
+  imports: [HeaderlogoComponent, ContacticonsComponent, CommonModule],
   templateUrl: './herosection.component.html',
   styleUrl: './herosection.component.scss',
 })
 export class HerosectionComponent {
   path = 'assets/img/Vector 29-1.png';
-  @Input() Isgerman: any;
+  @Input() Isgerman: boolean = false;
+  @ViewChild('imageprofile', { static: true }) image!: ElementRef<HTMLImageElement>;
+  @ViewChild('imagebox', { static: true }) imagebox!: ElementRef<HTMLDivElement>;
+
+  showImage: boolean = true; 
+
+  toggleImage() {
+    this.showImage = !this.showImage;
+  }
 }
