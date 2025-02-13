0b98f0509ad4a1526a15956ae35b0ecbcd9f7ec3
Ioannis Karakasidis <168634993+Ioannis-Karakasidis@users.noreply.github.com>
2025-02-13 07:25:34 +0100

    .

--- Portofolio/src/app/references/references.component.ts
+++ Portofolio/src/app/references/references.component.ts
@@ -1,32 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { LanguageService } from '../language.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-references',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './references.component.html',
-  styleUrl: './references.component.scss',
+  styleUrls: ['./references.component.scss'],
 })
 export class ReferencesComponent {
+  isgerman = inject(LanguageService);
   reference = [
     {
       title: '',
-      projekt: 'DA Bublle',
+      project: 'DA Bublle',
       description: '',
     },
     {
       title: 'T. Sabanovic',
-      projekt: 'Join',
+      project: 'Join',
       description:
         'Ioannis was a very reliable team member. What I particularly appreciated was how he always brought new ideas and quickly found solutions. Whenever tasks were assigned to him, he completed them reliably. Working with him was highly productive.',
     },
     {
       title: 'Maik Joe Sedlmaier',
-      projekt: 'Join',
+      project: 'Join',
       description:
         'Ioannis is a highly skilled developer with a strong focus on frontend development and user experience. His creativity, attention to detail, and ability to translate complex ideas into intuitive designs set him apart. Ioannis is a reliable and innovative team player who always delivers exceptional work.',
     },
   ];
 
-  
+  german = [
+    {
+      title: '',
+      project: 'DA Bubble',
+      description: '',
+    },
+    {
+      title: 'T. Sabanovic',
+      project: 'Join',
+      description:
+        'Ioannis war ein sehr zuverlässiges Teammitglied. Besonders geschätzt habe ich, wie er immer neue Ideen einbrachte und schnell Lösungen fand. Wann immer Aufgaben ihm zugewiesen wurden, erledigte er sie zuverlässig. Die Zusammenarbeit mit ihm war sehr produktiv.',
+    },
+    {
+      title: 'Maik Joe Sedlmaier',
+      project: 'Join',
+      description:
+        'Ioannis ist ein sehr fähiger Entwickler mit einem starken Fokus auf Frontend-Entwicklung und Benutzererfahrung. Seine Kreativität, Detailgenauigkeit und die Fähigkeit, komplexe Ideen in intuitive Designs umzusetzen, heben ihn hervor. Ioannis ist ein zuverlässiger und innovativer Teamplayer, der immer außergewöhnliche Arbeit liefert.',
+    },
+  ];
 }
