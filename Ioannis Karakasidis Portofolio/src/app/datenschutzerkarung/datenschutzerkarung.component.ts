220d77a9eb06c7c62cdbd2ee003a41cfbee3d005
Ioannis Karakasidis <168634993+Ioannis-Karakasidis@users.noreply.github.com>
2025-02-17 02:35:55 +0100

    .

--- Ioannis Karakasidis Portofolio/src/app/datenschutzerkarung/datenschutzerkarung.component.ts
+++ Ioannis Karakasidis Portofolio/src/app/datenschutzerkarung/datenschutzerkarung.component.ts
@@ -2,7 +2,6 @@ import { Component, inject } from '@angular/core';
 import { LanguageService } from '../language.service';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
-import { PrivacypolicyComponent } from '../privacypolicy/privacypolicy.component';
 
 @Component({
   selector: 'app-datenschutzerkarung',
