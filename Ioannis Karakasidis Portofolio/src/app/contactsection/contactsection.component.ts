c62eb6caf441fbcfd22b2de604ed581d1b3bd157
Ioannis Karakasidis <168634993+Ioannis-Karakasidis@users.noreply.github.com>
2025-02-18 04:03:09 +0100

    .

--- Ioannis Karakasidis Portofolio/src/app/contactsection/contactsection.component.ts
+++ Ioannis Karakasidis Portofolio/src/app/contactsection/contactsection.component.ts
@@ -10,16 +10,20 @@ import { RouterLink } from '@angular/router';
   standalone: true,
   imports: [FormsModule, CommonModule, RouterLink],
   templateUrl: './contactsection.component.html',
-  styleUrl: './contactsection.component.scss',
+  styleUrls: ['./contactsection.component.scss'],
 })
 export class ContactsectionComponent {
-  readpolicy = false;
+  clickedtimes = 0;
+  clicked = false;
   Checked = false;
+  error = false;
   width = '85%';
   http = inject(HttpClient);
   Email: string = 'gianniskarakasidhs@hotmail.com';
   isgerman = inject(LanguageService);
+
   @ViewChild('overlay') overlay!: ElementRef;
+  @ViewChild('sendbutton') sendbuttons!: ElementRef;
 
   contactData = {
     name: '',
@@ -39,36 +43,48 @@ export class ContactsectionComponent {
   };
 
   onSubmit(ngForm: NgForm) {
-    if (ngForm.submitted && ngForm.form.valid && this.readpolicy) {
+    if (ngForm.submitted && ngForm.form.valid && this.clicked) {
       this.http
         .post(this.post.endPoint, this.post.body(this.contactData))
         .subscribe({
           next: (response) => {
-            ngForm.resetForm();
+            console.log('Mail sent successfully:', response);
           },
           error: (error) => {
             console.error(error);
           },
           complete: () => {
-            ngForm.reset();
+            ngForm.resetForm();
             this.overlay.nativeElement.style.display = 'flex';
-
-            this.readpolicy = false;
             this.Checked = false;
+            this.clicked = false;
+            this.clickedtimes = 0;
+            this.error = false;
             setTimeout(() => {
               this.overlay.nativeElement.style.display = 'none';
             }, 1000);
           },
         });
+    } else {
+      this.clicked = true;
+      if(!this.Checked){
+        this.error = true;
+
+      }
     }
   }
 
   changepic() {
-    if (this.Checked === false) {
+    if (!this.Checked) {
       this.Checked = true;
-      this.readpolicy = true;
+      this.clicked = false;
+      this.error = false;
+      this.clickedtimes++;
+      this.sendbuttons.nativeElement.classList.add('active');
     } else {
       this.Checked = false;
+      this.clicked = false;
+      this.sendbuttons.nativeElement.classList.remove('active');
     }
   }
 }
--- Ioannis Karakasidis Portofolio/src/app/herosection/herosection.component.ts
+++ Ioannis Karakasidis Portofolio/src/app/herosection/herosection.component.ts
@@ -1,4 +1,11 @@
-import { Component, ElementRef, inject, Input, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  inject,
+  Input,
+  ViewChild,
+} from '@angular/core';
 import { HeaderlogoComponent } from '../headerlogo/headerlogo.component';
 import { ContacticonsComponent } from '../contacticons/contacticons.component';
 import { CommonModule } from '@angular/common';
@@ -12,13 +19,22 @@ import { LanguageService } from '../language.service';
   styleUrl: './herosection.component.scss',
 })
 export class HerosectionComponent {
+  windowWidth: number = window.innerWidth;
   path = 'assets/img/Vector 29-1.png';
   @ViewChild('imageprofile', { static: true })
   image!: ElementRef<HTMLImageElement>;
   @ViewChild('imagebox', { static: true })
   imagebox!: ElementRef<HTMLDivElement>;
-  Isgerman = inject(LanguageService)
+  Isgerman = inject(LanguageService);
   showImage: boolean = true;
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.windowWidth = event.target.innerWidth;
+  }
+
+  ngOnInit() {
+    this.windowWidth = window.innerWidth;
+  }
 
   toggleImage() {
     this.showImage = !this.showImage;
