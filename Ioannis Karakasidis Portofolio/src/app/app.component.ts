c3debe972299037548e2e1ef99d33c1fa1cfcee8
Ioannis Karakasidis <168634993+Ioannis-Karakasidis@users.noreply.github.com>
2025-02-19 00:11:12 +0100

    .

--- Ioannis Karakasidis Portofolio/src/app/app.component.ts
+++ Ioannis Karakasidis Portofolio/src/app/app.component.ts
@@ -1,16 +1,32 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink, RouterOutlet } from '@angular/router';
-
+import AOS from 'aos';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterOutlet,
-  ],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    // Initialize AOS once on component initialization
+    AOS.init();
+  }
+
+  // Drag-over handler to allow drop
+  onDragOver(event: DragEvent) {
+    event.preventDefault(); // This is required to allow the drop
+  }
+
+  // Drop handler
+  onDrop(event: DragEvent) {
+    event.preventDefault(); // Prevent default drop action
+    console.log('Drop event triggered');
+    
+    // Refresh AOS to trigger the "fade-right" animation
+    AOS.refresh();
+  }
+}
